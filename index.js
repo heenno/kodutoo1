a1e970407776aa511c4416b6e5b8575d85bf3036
Helen <helenents1@gmail.com>
2022-09-11 22:28:25 +0300

    make it better

--- index.js
+++ index.js
@@ -2,15 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 80;
 
-/*app.get('/', (req, res) => {
-    res.send('Hello World, from express');
-});
-app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
-*/
-
 app.get('/index', (req, res) => {
     console.log("Nimi:",req.query.nimi);
-    res.send('Tervituse saamiseks palun kirjuta URL-i lõppu peale = märki oma nimi.\r\n Tere ' + req.query.nimi);
+    res.send('Tervituse saamiseks palun kirjuta URL-i lõppu peale = märki oma nimi. Tere ' + req.query.nimi);
 });
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
